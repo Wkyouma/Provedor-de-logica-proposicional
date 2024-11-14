e5f8aded18ab5357d563918071e8ae8ad421fd87
Igor Terplak Gutierrez <135295319+Wkyouma@users.noreply.github.com>
2024-11-14 01:31:43 -0300

    Update main (6).hs

--- main (6).hs
+++ main (6).hs
@@ -1,4 +1,5 @@
--- Tipos de dados para os tokens e expressões proposicionais
+
+--tipos de tokens
 data Token
   = Var Char
   | Booleano Bool
@@ -11,32 +12,32 @@ data Token
   | RightParen
   deriving (Show, Eq)
 
--- Função para tokenizar uma string em tokens
-lexer :: String -> [Token]
-lexer [] = []
-lexer x
-  | "(" `prefixOf` x = LeftParen : lexer (drop 1 x)
-  | ")" `prefixOf` x = RightParen : lexer (drop 1 x)
-  | "v" `prefixOf` x || "∨" `prefixOf` x = Or : lexer (drop 1 x)
-  | "ou" `prefixOf` x || "or" `prefixOf` x = Or : lexer (drop 2 x)
-  | "\\lor" `prefixOf` x = Or : lexer (drop 4 x)
-  | "^" `prefixOf` x || "∧" `prefixOf` x = And : lexer (drop 1 x)
-  | "e" `prefixOf` x || "and" `prefixOf` x = And : lexer (drop 3 x)
-  | "\\land" `prefixOf` x = And : lexer (drop 5 x)
-  | "~" `prefixOf` x || "¬" `prefixOf` x = Not : lexer (drop 1 x)
-  | "not" `prefixOf` x = Not : lexer (drop 3 x)
-  | "\\neg" `prefixOf` x = Not : lexer (drop 4 x)
-  | "¬" `prefixOf` x = Not : lexer (drop 1 x)
-  | "->" `prefixOf` x || "=>" `prefixOf` x = Implication : lexer (drop 2 x)
-   | "→" `prefixOf` x = Implication : lexer (drop 1 x)
-  | "\\to" `prefixOf` x = Implication : lexer (drop 3 x)
-  | "<->" `prefixOf` x || "<=>" `prefixOf` x = Biconditional : lexer (drop 3 x)
-  | "↔" `prefixOf` x = Biconditional : lexer (drop 1 x)
-  | "\\Leftrightaarow" `prefixOf` x = Biconditional : lexer (drop 15 x)
-  | "true" `prefixOf` x = Booleano True : lexer (drop 4 x)
-  | "false" `prefixOf` x = Booleano False : lexer (drop 5 x)
-  | head x `elem` ['A' .. 'Z'] = Var (head x) : lexer (drop 1 x)
-  | head x == ' ' || head x == '\t' = lexer (drop 1 x)
+-- Função para transformar em token
+tokenize  :: String -> [Token]
+tokenize  [] = []
+tokenize  x
+  | "(" `prefixOf` x = LeftParen : tokenize  (drop 1 x)
+  | ")" `prefixOf` x = RightParen : tokenize  (drop 1 x)
+  | "v" `prefixOf` x || "∨" `prefixOf` x = Or : tokenize  (drop 1 x)
+  | "ou" `prefixOf` x || "or" `prefixOf` x = Or : tokenize  (drop 2 x)
+  | "\\lor" `prefixOf` x = Or : tokenize  (drop 4 x)
+  | "^" `prefixOf` x || "∧" `prefixOf` x = And : tokenize  (drop 1 x)
+  | "e" `prefixOf` x || "and" `prefixOf` x = And : tokenize  (drop 3 x)
+  | "\\land" `prefixOf` x = And : tokenize  (drop 5 x)
+  | "~" `prefixOf` x || "¬" `prefixOf` x = Not : tokenize  (drop 1 x)
+  | "not" `prefixOf` x = Not : tokenize  (drop 3 x)
+  | "\\neg" `prefixOf` x = Not : tokenize  (drop 4 x)
+  | "¬" `prefixOf` x = Not : tokenize  (drop 1 x)
+  | "->" `prefixOf` x || "=>" `prefixOf` x = Implication : tokenize  (drop 2 x)
+  | "→" `prefixOf` x = Implication : tokenize  (drop 1 x)
+  | "\\to" `prefixOf` x = Implication : tokenize  (drop 3 x)
+  | "<->" `prefixOf` x || "<=>" `prefixOf` x = Biconditional : tokenize  (drop 3 x)
+  | "↔" `prefixOf` x = Biconditional : tokenize  (drop 1 x)
+  | "\\iff" `prefixOf` x = Biconditional : tokenize  (drop 4 x)
+  | "true" `prefixOf` x = Booleano True : tokenize  (drop 4 x)
+  | "false" `prefixOf` x = Booleano False : tokenize  (drop 5 x)
+  | head x `elem` ['A' .. 'Z'] = Var (head x) : tokenize  (drop 1 x)
+  | head x == ' ' || head x == '\t' = tokenize  (drop 1 x)
   | otherwise = error $ "Caractere inválido: " ++ [head x]
 
 prefixOf :: String -> String -> Bool
@@ -46,13 +47,13 @@ prefixOf (x:xs) (y:ys) = x == y && prefixOf xs ys
 
 -- Precedência dos operadores
 precedencia :: Token -> Int
-precedencia Not = 6
-precedencia And = 5
-precedencia Or = 4
-precedencia Implication = 3
-precedencia Biconditional = 2
-precedencia LeftParen = 1
-precedencia RightParen = 1
+precedencia Not = 5
+precedencia And = 4
+precedencia Or = 3
+precedencia Implication = 2
+precedencia Biconditional = 1
+precedencia LeftParen = 0
+precedencia RightParen = 0
 
 -- Função para converter expressão infixa para notação pós-fixa (RPN)
 ordenar :: [Token] -> [Token] -> [Token] -> [Token]
@@ -80,7 +81,7 @@ classificar expr =
 -- Avaliação da expressão para todas as combinações de valores booleanos das variáveis
 avaliarExpressao :: String -> [Bool]
 avaliarExpressao expr =
-  let tokens = lexer expr
+  let tokens = tokenize expr
       vars = variaveis tokens
       combinacoes = sequence (replicate (length vars) [True, False])
       rpn = ordenar tokens [] []
@@ -118,46 +119,45 @@ evalExpr vars = eval []
       (s1:s2:rest) -> eval (((s2 && s1) || (not s2 && not s1)) : rest) xs
       _ -> error "Pilha insuficiente para operador 'Biconditional'"
     eval _ _ = error "Expressão inválida"
-
-data Prop
+-- Tipo de dado para representar expressões proposicionais
+data Proposicao
   = PVar Char
   | PBool Bool
-  | PNao Prop
-  | PConj Prop Prop
-  | PDisj Prop Prop
-  | PImpli Prop Prop
-  | PBiCon Prop Prop
+  | PNao Proposicao
+  | PConj Proposicao Proposicao
+  | PDisj Proposicao Proposicao
+  | PImpli Proposicao Proposicao
+  | PBiCon Proposicao Proposicao
   deriving (Eq, Show)
 
 -- Eliminar implicações e bicondicionais
-elimImpli :: Prop -> Prop
-elimImpli (PVar x) = PVar x
-elimImpli (PBool b) = PBool b
-elimImpli (PNao x) = PNao (elimImpli x)
-elimImpli (PConj x y) = PConj (elimImpli x) (elimImpli y)
-elimImpli (PDisj x y) = PDisj (elimImpli x) (elimImpli y)
-elimImpli (PImpli x y) = PDisj (PNao (elimImpli x)) (elimImpli y)
-elimImpli (PBiCon x y) = PConj (elimImpli (PImpli x y)) (elimImpli (PImpli y x))
-
+transImplic :: Proposicao -> Proposicao
+transImplic (PVar x) = PVar x
+transImplic (PBool b) = PBool b
+transImplic (PNao x) = PNao (transImplic x)
+transImplic (PConj x y) = PConj (transImplic x) (transImplic y)
+transImplic (PDisj x y) = PDisj (transImplic x) (transImplic y)
+transImplic (PImpli x y) = PDisj (PNao (transImplic x)) (transImplic y)
+transImplic (PBiCon x y) = PConj (transImplic (PImpli x y)) (transImplic (PImpli y x))
 
 -- Mover negações para dentro e simplificar dupla negação
-elimNeg :: Prop -> Prop
-elimNeg (PVar x) = PVar x
-elimNeg (PBool b) = PBool b
-elimNeg (PNao (PNao x)) = elimNeg x  -- Elimina dupla negação
-elimNeg (PNao (PConj x y)) = PDisj (elimNeg (PNao x)) (elimNeg (PNao y))  -- De Morgan
-elimNeg (PNao (PDisj x y)) = PConj (elimNeg (PNao x)) (elimNeg (PNao y))  -- De Morgan
-elimNeg (PNao x) = PNao (elimNeg x)
-elimNeg (PConj x y) = PConj (elimNeg x) (elimNeg y)
-elimNeg (PDisj x y) = PDisj (elimNeg x) (elimNeg y)
+transNeg :: Proposicao -> Proposicao
+transNeg (PVar x) = PVar x
+transNeg (PBool b) = PBool b
+transNeg (PNao (PNao x)) = transNeg x  -- Elimina dupla negação
+transNeg (PNao (PConj x y)) = PDisj (transNeg (PNao x)) (transNeg (PNao y))  -- De Morgan
+transNeg (PNao (PDisj x y)) = PConj (transNeg (PNao x)) (transNeg (PNao y))  -- De Morgan
+transNeg (PNao x) = PNao (transNeg x)
+transNeg (PConj x y) = PConj (transNeg x) (transNeg y)
+transNeg (PDisj x y) = PDisj (transNeg x) (transNeg y)
 -- Distribuir disjunções sobre conjunções para obter a FNC
-distributivaProp :: Prop -> Prop
+distributivaProp :: Proposicao -> Proposicao
 distributivaProp p =
   let p' = distribDisj p
   in if p == p' then p else distributivaProp p'
 
 -- Distribuição parcial das disjunções sobre conjunções
-distribDisj :: Prop -> Prop
+distribDisj :: Proposicao -> Proposicao
 distribDisj (PConj x y) = PConj (distribDisj x) (distribDisj y)
 distribDisj (PDisj (PConj x y) z) = PConj (distribDisj (PDisj x z)) (distribDisj (PDisj y z))
 distribDisj (PDisj z (PConj x y)) = PConj (distribDisj (PDisj z x)) (distribDisj (PDisj z y))
@@ -167,10 +167,10 @@ distribDisj (PVar x) = PVar x
 distribDisj (PBool x) = PBool x
 
 -- Função principal para converter uma expressão para FNC
-converterParaFNC :: Prop -> Prop
-converterParaFNC = distributivaProp . elimNeg . elimImpli
+converterParaFNC :: Proposicao -> Proposicao
+converterParaFNC = distributivaProp . transNeg . transImplic
 
-parseRPN :: [Token] -> Prop
+parseRPN :: [Token] -> Proposicao
 parseRPN = parse []
   where
     parse [result] [] = result
@@ -181,30 +181,30 @@ parseRPN = parse []
     parse (y : x : xs) (Or : rest) = parse (PDisj x y : xs) rest
     parse (y : x : xs) (Implication : rest) = parse (PImpli x y : xs) rest
     parse (y : x : xs) (Biconditional : rest) = parse (PBiCon x y : xs) rest
-    parse _ _ = error "Expressão inválida para conversão em Prop"
+    parse _ _ = error "Expressão inválida para conversão em Proposicao"
     
 -- Função para verificar se uma disjunção é uma cláusula de Horn
-ehClausulaHorn :: Prop -> Bool
-ehClausulaHorn (PDisj x y) = numPositivos (PDisj x y) <= 1
-ehClausulaHorn x = True  -- Um único literal é sempre uma cláusula de Horn
+posiClausulaHorn :: Proposicao -> Bool
+posiClausulaHorn (PDisj x y) = numPosi (PDisj x y) <= 1
+posiClausulaHorn x = True  -- Um único literal é sempre uma cláusula de Horn
 
 -- Contagem de literais positivos em uma expressão
-numPositivos :: Prop -> Int
-numPositivos (PVar _) = 1
-numPositivos (PNao _) = 0
-numPositivos (PDisj x y) = numPositivos x + numPositivos y
-numPositivos _ = 0
+numPosi:: Proposicao -> Int
+numPosi (PVar _) = 1
+numPosi (PNao _) = 0
+numPosi (PDisj x y) = numPosi x + numPosi y
+numPosi _ = 0
 
 -- Separar as cláusulas da FNC
-extrairClausulas :: Prop -> [Prop]
+extrairClausulas :: Proposicao -> [Proposicao]
 extrairClausulas (PConj x y) = extrairClausulas x ++ extrairClausulas y
 extrairClausulas clausula = [clausula]
 
 -- Exibir as cláusulas de Horn ou informar se não for possível
-exibirClausulasHorn :: Prop -> IO ()
+exibirClausulasHorn :: Proposicao -> IO ()
 exibirClausulasHorn fnc = 
   let clausulas = extrairClausulas fnc
-      clausulasHorn = filter ehClausulaHorn clausulas
+      clausulasHorn = filter posiClausulaHorn clausulas
   in if length clausulas == length clausulasHorn
      then do
        putStrLn "As cláusulas de Horn resultantes são:"
@@ -214,20 +214,36 @@ exibirClausulasHorn fnc =
 
 printSeparator :: Char -> Int -> IO ()
 printSeparator c n = putStrLn (replicate n c)
+-- Função auxiliar para converter expressão Proposicao para LaTeX e terminal
+toLatex :: Proposicao -> String
+toLatex (PVar x) = [x]
+toLatex (PBool True) = "true"
+toLatex (PBool False) = "false"
+toLatex (PNao x) = "\\neg " ++ toLatex x
+toLatex (PConj x y) = "(" ++ toLatex x ++ " \\land " ++ toLatex y ++ ")"
+toLatex (PDisj x y) = "(" ++ toLatex x ++ " \\lor " ++ toLatex y ++ ")"
+toLatex (PImpli x y) = "(" ++ toLatex x ++ " \\to " ++ toLatex y ++ ")"
+toLatex (PBiCon x y) = "(" ++ toLatex x ++ " \\leftrightarrow " ++ toLatex y ++ ")"
+
+-- Função para exibir a expressão em LaTeX e no terminal
+printLaTeX :: String -> Proposicao -> IO ()
+printLaTeX msg expr = do
+    putStrLn $ msg ++ " (Terminal): " ++ show expr
+    putStrLn $ msg ++ " (LaTeX): $$" ++ toLatex expr ++ "$$"
 
 main :: IO ()
 main = do
-    let str = "P \\Leftrightaarow Q"
-    let lexado = lexer str
-    print $ "Tokens: " ++ show lexado
+    let str = "P v (Q ^~Q) <-> P"
+    let lexado = tokenize  str
+    putStrLn $ "Tokens: " ++ show lexado
     printSeparator '=' 100
     let rpn = ordenar lexado [] []
-    print $ "Notacao Pos-Fixa (RPN): " ++ show rpn
-    putStrLn $ "Classificacao da expressao: " ++ classificar str
+    putStrLn $ "Notacaoo Pos-Fixa (RPN): " ++ show rpn
+    putStrLn $ "Classificação da expressão: " ++ classificar str
     printSeparator '=' 100
-    let propExpr = parseRPN rpn
-    print $ "Expressao Prop: " ++ show propExpr
-    let fnc = converterParaFNC propExpr
-    print $ "Expressao em FNC: " ++ show fnc
+    let prop = parseRPN rpn
+    printLaTeX "Expressão Proposicional" prop
+    let fnc = converterParaFNC prop
+    printLaTeX "Expressão em FNC" fnc
     printSeparator '=' 100
     exibirClausulasHorn fnc
